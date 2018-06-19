01e285f91608f2fa3e604cb995930dc98b70ace5
Jhonattas Ferreira <jfs4@cin.ufpe.br>
2018-06-19 00:29:08 -0300

    partial: Cria projeto react e cria input parcial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <div class="col-md-12">
+        <form>        
+            <input type="text" name="name" />        
+        </form>
+    </div>,
+    document.getElementById('root')
+  );
