b7ac719f78bb1c14c5e6d569b6974c3c28520eeb
Jhonattas Ferreira <jfs4@cin.ufpe.br>
2018-06-22 23:35:41 -0300

    feat: Cria componente de tempo medio de pull request

--- src/components/PullRequestMergeTime.js
+++ src/components/PullRequestMergeTime.js
@@ -1,21 +1,82 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class PullRequestMergeTime extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {gitHubUrl: props.url};
+        this.state = {day: 0, hour: 0, minute: 0,flag:true};
     }
 
     componentDidUpdate() {
-        if(this.props.url != null && this.props.url != ''){
-            var data = this.getPullRequestData(this.props.url);
+        var self = this;
+        if(this.props.url != null && this.props.url != '' && self.state.flag) {
+            self.setState({flag: false})       
+            let promisse = this.getPullRequestData(this.props.url,self);
+            promisse.then(result => {
+                
+            });
         }
     }
 
-    getPullRequestData(url) {
+    ///pulls?state=closed&page=" + page + "&per_page=100"
+    getPullRequestData(url,self) {
 
+        return new Promise((resolve, reject) =>{
+            let page = 1;
+            let quantity = 0;
+            let totalHours = 0;
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET",url + "/pulls?state=closed&page=" + page + "&per_page=100");
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if(xhr.status == 200){
+                        let dataPullRequest = JSON.parse(xhr.responseText);
+
+                        if (dataPullRequest != null && dataPullRequest.length > 0) {
+                            quantity = quantity + dataPullRequest.length;
+
+                            dataPullRequest.forEach(pullRequest => {
+                                let createdDate = new Date(pullRequest.created_at);
+                                let endDate = new Date(pullRequest.merged_at);
+                                if(pullRequest.merged_at == null){
+                                    endDate = new Date(pullRequest.closed_at);
+                                }
+                                let timeDiff = Math.abs(endDate.getTime() - createdDate.getTime());
+                                let diffhours = timeDiff/1000/60/60;
+
+                                totalHours = totalHours + diffhours;
+
+                            });
+
+                            page = page + 1;
+                            xhr.open("GET", url + "/pulls?state=closed&page=" + page + "&per_page=100");
+                            xhr.send();
+                        }
+                        else{
+                            let average = totalHours/quantity;
+                            let Days=Math.floor(average/24);
+                            let Remainder=average % 24;
+                            let Hours=Math.floor(Remainder);
+                            let Minutes=Math.floor(60*(Remainder-Hours));
+                            let averagePullRequestTime = {"Days":Days,"Hours":Hours,"Minutes":Minutes}
+
+                            let output = {
+                                averagePullRequestTime: averagePullRequestTime,
+                                self: self  
+                            }
+
+                            self.setState({day: averagePullRequestTime.Days});
+                            self.setState({hour: averagePullRequestTime.Hours});
+                            self.setState({minute: averagePullRequestTime.Minutes});
+                            resolve(output);
+                        } 
+                    }
+                }
+            }
+
+            xhr.send();
+        });
     }
 
     render(){
@@ -26,6 +87,10 @@ export default class PullRequestMergeTime extends React.Component {
                     <span class="text-head">Average Pull Request Merge Time</span>
                 </div>
                 <div class="body-component-6">
+                    <div class="information-average">
+                        <span>{this.state.day} day </span>
+                        <span>{this.state.hour}h{this.state.minute}m</span>                        
+                    </div>
                 </div>
             </div>            
         </div>
